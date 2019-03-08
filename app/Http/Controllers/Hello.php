679ef053788f07f2129c51c9930d255f14fd07ca
Danny Gershman <danny.gershman@gmail.com>
2019-03-08 07:59:48 -0500

    parameterized test

--- app/Http/Controllers/Hello.php
+++ app/Http/Controllers/Hello.php
@@ -6,8 +6,8 @@ use Illuminate\Http\Request;
 
 class Hello extends Controller
 {
-    public function index()
+    public function index($name)
     {
-        return 'hello world!';
+        return view('hello', ['name' => $name]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', 'Hello@index');
+Route::get('/hello/{name}', 'Hello@index');
