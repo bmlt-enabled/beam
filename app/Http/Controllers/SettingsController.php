f3e6c37d489273940ecb062254157137ff954dae
Danny Gershman <danny.gershman@gmail.com>
2019-03-20 23:40:51 -0400

    show basic settings page

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Beam;
 use Illuminate\Http\Request;
 
 class SettingsController extends Controller
@@ -13,6 +14,7 @@ class SettingsController extends Controller
 
     public function index()
     {
-        return view('settings');
+        $beams = Beam::all();
+        return view('settings', ['beams'=>$beams]);
     }
 }
--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -5,14 +5,33 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Settings</div>
-
+                    <div class="card-header"></div>
+                    <div class="card-body">
+                        <table border="1">
+                            <tr>
+                                <th>Name</th>
+                                <th>Url</th>
+                                <th>Public Key</th>
+                                <th>Private Key</th>
+                            </tr>
+                            @foreach ($beams as $beam)
+                            <tr>
+                                <td>{{ $beam->name }}</td>
+                                <td>{{ $beam->url }}</td>
+                                <td>{{ md5('red') }}</td>
+                                <td>{{ md5('blue') }}</td>
+                            </tr>
+                            @endforeach
+                        </table>
+                    </div>
+                    <div class="card-footer"></div>
 
                     <div class="card-header">Cache Flush</div>
                     <div class="card-body">
                         <button class="btn btn-sm btn-danger" onclick="location.href='/flush/service_bodies'">Service Bodies</button>
                         <button class="btn btn-sm btn-danger" onclick="location.href='/flush/beamed_users'">Beamed Users</button>
                     </div>
+                    <div class="card-footer"></div>
                 </div>
             </div>
         </div>
