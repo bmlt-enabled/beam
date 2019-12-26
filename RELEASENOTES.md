7479fc110fd4dfa2f1dc15010dbe4c4c7e227c0a
Danny <danny.gershman@gmail.com>
2019-12-25 21:38:33 -0500

    bump to 0.1.1

---
+++ RELEASENOTES.md
@@ -0,0 +1,7 @@
+# Release Notes
+
+### 0.1.1
+* Bundle fixes for artifact
+
+### 0.1.0
+* Deep link redirection for posts after login.
--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     |
     */
-    'version' => '0.1.0',
+    'version' => '0.1.1',
     'name' => env('APP_NAME', 'beam'),
     'bmlt_root_server' => env('BMLT_ROOT_SERVER'),
 
